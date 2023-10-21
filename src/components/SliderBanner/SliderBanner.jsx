32b1c4cc857835a9ef90d41fd9d9b27d080758c1
Coder Emran <coderemran6910@gmail.com>
2023-10-21 09:39:34 +0600

    added Slider Image and responsive for all device

--- src/components/SliderBanner/SliderBanner.jsx
+++ src/components/SliderBanner/SliderBanner.jsx
@@ -30,15 +30,15 @@ export default function App() {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-       <SwiperSlide> <img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide> <img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
-        <SwiperSlide><img src="https://i.ibb.co/0BJFdGy/anh-nhat-yqclo-Mb3-Abw-unsplash.jpg" alt="" /> </SwiperSlide>
+       <SwiperSlide> <img  src="https://i.ibb.co/3yt6KXh/alex-presa-At-Xt2o-Lq-Yns-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide> <img src="https://i.ibb.co/p1P401y/jonas-leupe-Ggw9-Tkdr-AA0-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/3Yt869g/martin-katler-7w-Cxl-Bf-GMdk-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/VBrdFPP/nikita-kostrykin-i1-Q9pphr-WUw-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/ZTxX5Ny/conor-luddy-IVa-Kks-EZm-ZA-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/pdVPJFN/daniel-korpai-OI0-G3-K6-KVM-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/MkWz2Gb/zana-latif-QCBy84h-Le-JU-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/njpQXgz/alexandru-bogdan-ghita-e-M6-WUs4n-KMY-unsplash.jpg" alt="" /> </SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/Xkp4z8Y/khoa-nguyen-Jdp-Gvuy1n-RQ-unsplash.jpg" alt="" /> </SwiperSlide>
       </Swiper>
     </>
   );
